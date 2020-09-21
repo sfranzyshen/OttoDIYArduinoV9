f4acdfdccff1d624ae2e9b36bcb63fe9d57af942
Scotty Franzyshen <sfranzyshen@gmail.com>
2020-09-21 16:01:23 -0700

    Update Otto9.h

--- Otto9.h
+++ Otto9.h
@@ -1,7 +1,4 @@
 // OttoDIY version 0.3.9
-#ifndef Otto9_h
-  #define Otto9_h
-#endif
 
 #include <Servo.h>
 #include <Oscillator.h>
@@ -20,25 +17,20 @@ class Otto9 {
   public:
     // Otto initialization
     void init(int YL, int YR, int RL, int RR, bool load_calibration, int NoiseSensor, int Buzzer, int USTrigger, int USEcho);
-
     // Attach & detach functions
     void attachServos();
     void detachServos();
-
     // Oscillator Trims
     void setTrims(int YL, int YR, int RL, int RR);
     void saveTrimsOnEEPROM();
-
     // Predetermined Motion Functions
     void moveServos(int time, int servo_target[]);
     void moveSingle(int position,int servo_number);
     void oscillateServos(int A[4], int O[4], int T, double phase_diff[4], float cycle);
-
     // HOME = Otto at rest position
     void home();
     bool getRestState();
     void setRestState(bool state);
-    
     // Predetermined Motion Functions
     void jump(float steps = 1, int T = 2000);
     void walk(float steps = 4, int T = 1000, int dir = FORWARD);
@@ -53,7 +45,6 @@ class Otto9 {
     void moonwalker(float steps = 1, int T = 900, int h = 20, int dir = LEFT);
     void crusaito(float steps = 1, int T = 900, int h = 20, int dir = FORWARD);
     void flapping(float steps = 1, int T = 1000, int h = 20, int dir = FORWARD);
-
   private:
     Oscillator _servo[4];
     int _servo_pins[4];
