22dc43882dd14674cb2b14170e62422e01272707
Scotty Franzyshen <sfranzyshen@gmail.com>
2020-09-22 16:09:11 -0700

    Update Otto_allmoves_V9.ino

--- examples/Otto_allmoves_V9.ino
+++ examples/Otto_allmoves_V9.ino
@@ -1,19 +1,26 @@
 /*
-Otto All moves Arduin test 
+Otto All moves Arduino test 
 OttoDIY Arduino Project, 2020 | sfranzyshen
 */
 
-#include <Otto9.h>
-Otto9 Otto; // This is Otto!
+#include <Otto9.h> //-- Otto Library version 9
+Otto9 Otto;  //This is Otto!
 
+int bend1[] = {90, 90, 62, 35}; 
+int bend2[] = {30, 10, 62, 105};
+  
 void setup() {
+  Serial.begin(115200);
   //Otto.init(D1, D6, D7, D5, false, 0, 1, 2, 3);
   Otto.init(2, 3, 4, 5, false, 0, 1, 2, 3);
   Otto.home();
 }
 
 void loop() {
-  Otto.walk(2, 1000, 1);           // 2 steps, "TIME". IF HIGHER THE VALUE THEN SLOWER (from 600 to 1400), 1 FORWARD
+  Otto.moveServos(1000, bend1);
+  Otto.moveServos(3000, bend2);
+  /*
+  Otto.walk(1, 3000, 1);           // 2 steps, "TIME". IF HIGHER THE VALUE THEN SLOWER (from 600 to 1400), 1 FORWARD
   Otto.walk(2, 1000, -1);          // 2 steps, T, -1 BACKWARD 
   Otto.turn(2, 1000, 1);            // 3 steps turning LEFT
   Otto.home();
@@ -40,4 +47,6 @@ void loop() {
   Otto.ascendingTurn(2, 1000, 50);
   Otto.jump(1, 2000);
   Otto.home();
+  */
+  Serial.println("Finished");
 }
