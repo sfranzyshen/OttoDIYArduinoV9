a4ecd50299007000e0b265e278dcbce2c9fd54db
Scotty Franzyshen <sfranzyshen@gmail.com>
2020-09-21 20:55:23 -0700

    Update Otto9.cpp
    thought so ...

--- Otto9.cpp
+++ Otto9.cpp
@@ -123,8 +123,8 @@ void Otto9::_execute(int A[], int O[], int T, double phase_diff[], float steps) 
 // HOME = Otto at rest position
 void Otto9::home() {
   if(getRestState() == false) { // Go to rest position only if necessary
-    //int homes[] = {90, 90, 90, 90}; //All the servos at rest position
-    moveServos(500, (int)[90, 90, 90, 90]); //Move the servos in half a second
+    int homes[] = {90, 90, 90, 90}; //All the servos at rest position
+    moveServos(500, homes); //Move the servos in half a second
     detachServos();
     setRestState(true);
   }
@@ -145,12 +145,10 @@ void Otto9::setRestState(bool state) {
 //    steps: Number of steps
 //    T: Period
 void Otto9::jump(float steps, int T) {
-//  int up[] = {90, 90, 150, 30};
-//  moveServos(T, up);
-  moveServos(T, (int)[90, 90, 150, 30]);
-//  int down[] = {90, 90, 90, 90};
-//  _moveServos(T, down);
-  moveServos(T, (int)[90, 90, 90, 90]);
+  int up[] = {90, 90, 150, 30};
+  moveServos(T, up);
+  int down[] = {90, 90, 90, 90};
+  moveServos(T, down);
 }
 
 // Otto gait: Walking  (forward or backward)    
@@ -166,12 +164,12 @@ void Otto9::walk(float steps, int T, int dir) {
   //      -90 : Walk forward
   //       90 : Walk backward
   // Feet servos also have the same offset (for tiptoe a little bit)
-//  int A[] = {30, 30, 20, 20};
-//  int O[] = {0, 0, 4, -4};
-//  double phase_diff[] = {0, 0, DEG2RAD(dir * -90), DEG2RAD(dir * -90)};
+  int A[] = {30, 30, 20, 20};
+  int O[] = {0, 0, 4, -4};
+  double phase_diff[] = {0, 0, DEG2RAD(dir * -90), DEG2RAD(dir * -90)};
 
   //-- Let's oscillate the servos!
-  _execute((int)[30, 30, 20, 20], (int)[0, 0, 4, -4], T, (double)[0, 0, DEG2RAD(dir * -90), DEG2RAD(dir * -90)], steps);  
+  _execute(A, O, T, phase_diff, steps);  
 }
 
 // Otto gait: Turning (left or right)
@@ -185,8 +183,8 @@ void Otto9::turn(float steps, int T, int dir) {
   // When the right hip servo amplitude is higher, the steps taken by
   //   the right leg are bigger than the left. So, the robot describes an left arc
   int A[] = {30, 30, 20, 20};
-  //int O[4] = {0, 0, 4, -4};
-  //double phase_diff[4] = {0, 0, DEG2RAD(-90), DEG2RAD(-90)}; 
+  int O[4] = {0, 0, 4, -4};
+  double phase_diff[4] = {0, 0, DEG2RAD(-90), DEG2RAD(-90)}; 
   if(dir == LEFT) {  
     A[0] = 30; // Left hip servo
     A[1] = 10; // Right hip servo
@@ -195,7 +193,7 @@ void Otto9::turn(float steps, int T, int dir) {
     A[1] = 30;
   }
   // Let's oscillate the servos!
-  _execute(A, (int)[0, 0, 4, -4], T, (double)[0, 0, DEG2RAD(-90), DEG2RAD(-90)], steps); 
+  _execute(A, O, T, phase_diff, steps); 
 }
 
 // Otto gait: Lateral bend
@@ -208,16 +206,15 @@ void Otto9::bend(int steps, int T, int dir) {
   int bend1[] = {90, 90, 62, 35}; 
   int bend2[] = {90, 90, 62, 105};
   int homes[] = {90, 90, 90, 90};
-  //Time of one bend, constrained in order to avoid movements too fast.
+  // Time of one bend, constrained in order to avoid movements too fast.
   //T=max(T, 600);
-  //Changes in the parameters if right direction is chosen 
   if(dir == RIGHT) { // Changes in the parameters if right direction is chosen 
     bend1[2] = 145;
     bend1[3] = 120;
     bend2[2] = 75;
     bend2[3] = 120;
   }
-  //Time of the bend movement. Fixed parameter to avoid falls
+  // Time of the bend movement. Fixed parameter to avoid falls
   int T2 = 800; 
   //Bend movement
   for(int i=0; i < steps; i++) {
@@ -240,7 +237,7 @@ void Otto9::shakeLeg(int steps, int T,int dir) {
   int shake_leg1[] = {90, 90, 58, 35};   
   int shake_leg2[] = {90, 90, 58, 120};
   int shake_leg3[] = {90, 90, 58, 60};
-  //int homes[] = {90, 90, 90, 90};
+  int homes[] = {90, 90, 90, 90};
   // Changes in the parameters if right leg is chosen
   if(dir == RIGHT) {
     shake_leg1[2] = 145;
@@ -264,7 +261,7 @@ void Otto9::shakeLeg(int steps, int T,int dir) {
       moveServos(T / (2 * numberLegMoves), shake_leg3);
       moveServos(T / (2 * numberLegMoves), shake_leg2);
     }
-    moveServos(500, (int)[90, 90, 90, 90]); //Return to home position
+    moveServos(500, homes); //Return to home position
   }
   delay(T);
 }
@@ -280,13 +277,13 @@ void Otto9::updown(float steps, int T, int h) {
   // Feet amplitude and offset are the same
   // Initial phase for the right foot is -90, so that it starts
   //   in one extreme position (not in the middle)
-  //int A[4]= {0, 0, h, h};
-  //int O[4] = {0, 0, h, -h};
-  //double phase_diff[4] = {0, 0, DEG2RAD(-90), DEG2RAD(90)};
+  int A[4]= {0, 0, h, h};
+  int O[4] = {0, 0, h, -h};
+  double phase_diff[4] = {0, 0, DEG2RAD(-90), DEG2RAD(90)};
   
   //-- Let's oscillate the servos!
   //_execute(A, O, T, phase_diff, steps); 
-  _execute((int)[0, 0, h, h], (int)[0, 0, h, -h], T, (double)[0, 0, DEG2RAD(-90), DEG2RAD(90)], steps); 
+  _execute(A, O, T, phase_diff, steps); 
 }
 
 // Otto movement: swinging side to side
@@ -298,12 +295,12 @@ void Otto9::swing(float steps, int T, int h) {
 
   // Both feets are in phase. The offset is half the amplitude
   // It causes the robot to swing from side to side
-  //int A[4]= {0, 0, h, h};
-  //int O[4] = {0, 0, h/2, -h/2};
-  //double phase_diff[4] = {0, 0, DEG2RAD(0), DEG2RAD(0)};
+  int A[4]= {0, 0, h, h};
+  int O[4] = {0, 0, h/2, -h/2};
+  double phase_diff[4] = {0, 0, DEG2RAD(0), DEG2RAD(0)};
   
   // Let's oscillate the servos!
-  _execute((int)[0, 0, h, h], (int)[0, 0, h / 2, -h / 2], T, (double)[0, 0, DEG2RAD(0), DEG2RAD(0)], steps); 
+  _execute(A, O, T, phase_diff, steps); 
 }
 
 // Otto movement: swinging side to side without touching the floor with the heel
@@ -314,12 +311,12 @@ void Otto9::swing(float steps, int T, int h) {
 void Otto9::tiptoeSwing(float steps, int T, int h) {
   // Both feets are in phase. The offset is not half the amplitude in order to tiptoe
   // It causes the robot to swing from side to side
-  //int A[4]= {0, 0, h, h};
-  //int O[4] = {0, 0, h, -h};
-  //double phase_diff[4] = {0, 0, 0, 0};
+  int A[4]= {0, 0, h, h};
+  int O[4] = {0, 0, h, -h};
+  double phase_diff[4] = {0, 0, 0, 0};
   
   //-- Let's oscillate the servos!
-  _execute((int)[0, 0, h, h], (int)[0, 0, h, -h], T, (double)[0, 0, 0, 0], steps); 
+  _execute(A, O, T, phase_diff, steps); 
 }
 
 // Otto gait: Jitter 
@@ -334,12 +331,12 @@ void Otto9::jitter(float steps, int T, int h) {
   //   in one extreme position (not in the middle)
   // h is constrained to avoid hit the feets
   h = min(25, h);
-  //int A[4]= {h, h, 0, 0};
-  //int O[4] = {0, 0, 0, 0};
-  //double phase_diff[4] = {DEG2RAD(-90), DEG2RAD(90), 0, 0};
+  int A[4]= {h, h, 0, 0};
+  int O[4] = {0, 0, 0, 0};
+  double phase_diff[4] = {DEG2RAD(-90), DEG2RAD(90), 0, 0};
   
   // Let's oscillate the servos!
-  _execute((int)[h, h, 0, 0], (int)[0, 0, 0, 0], T, (double)[DEG2RAD(-90), DEG2RAD(90), 0, 0], steps); 
+  _execute(A, O, T, phase_diff, steps); 
 }
 
 // Otto gait: Ascending & turn (Jitter while up&down)
@@ -353,12 +350,12 @@ void Otto9::ascendingTurn(float steps, int T, int h) {
   //   in one extreme position (not in the middle)
   // h is constrained to avoid hit the feets
   h = min(13, h);
-  //int A[4]= {h, h, h, h};
-  //int O[4] = {0, 0, h+4, -h+4};
-  //double phase_diff[4] = {DEG2RAD(-90), DEG2RAD(90), DEG2RAD(-90), DEG2RAD(90)};
+  int A[4]= {h, h, h, h};
+  int O[4] = {0, 0, h+4, -h+4};
+  double phase_diff[4] = {DEG2RAD(-90), DEG2RAD(90), DEG2RAD(-90), DEG2RAD(90)};
   
   // Let's oscillate the servos!
-  _execute((int)[h, h, h, h], (int)[0, 0, h + 4, -h + 4], T, (double)[DEG2RAD(-90), DEG2RAD(90), DEG2RAD(-90), DEG2RAD(90)], steps); 
+  _execute(A, O, T, phase_diff, steps); 
 }
 
 // Otto gait: Moonwalker. Otto moves like Michael Jackson
@@ -378,13 +375,13 @@ void Otto9::moonwalker(float steps, int T, int h, int dir) {
   //  Both amplitudes are equal. The offset is half the amplitud plus a little bit of
   //  offset so that the robot tiptoe lightly
  
-  //int A[4]= {0, 0, h, h};
-  //int O[4] = {0, 0, h/2+2, -h/2 -2};
-  //int phi = (-dir * 90);
-  //double phase_diff[4] = {0, 0, DEG2RAD(-dir * 90), DEG2RAD(-60 * dir + (-dir * 90))};
+  int A[4]= {0, 0, h, h};
+  int O[4] = {0, 0, h/2+2, -h/2 -2};
+  int phi = (-dir * 90);
+  double phase_diff[4] = {0, 0, DEG2RAD(phi), DEG2RAD(-60 * dir + phi)};
   
   //-- Let's oscillate the servos!
-  _execute((int)[0, 0, h, h], (int)[0, 0, h / 2 + 2, -h / 2 - 2], T, (double)[0, 0, DEG2RAD(-dir * 90), DEG2RAD(-60 * dir + (-dir * 90))], steps); 
+  _execute(A, O, T, phase_diff, steps); 
 }
 
 // Otto gait: Crusaito. A mixture between moonwalker and walk
@@ -394,12 +391,12 @@ void Otto9::moonwalker(float steps, int T, int h, int dir) {
 //     h: height (Values between 20 - 50)
 //     dir:  Direction: LEFT / RIGHT
 void Otto9::crusaito(float steps, int T, int h, int dir) {
-  //int A[4]= {25, 25, h, h};
-  //int O[4] = {0, 0, h/2+ 4, -h/2 - 4};
-  //double phase_diff[4] = {90, 90, DEG2RAD(0), DEG2RAD(-60 * dir)};
+  int A[4]= {25, 25, h, h};
+  int O[4] = {0, 0, h/2+ 4, -h/2 - 4};
+  double phase_diff[4] = {90, 90, DEG2RAD(0), DEG2RAD(-60 * dir)};
   
   //-- Let's oscillate the servos!
-  _execute((int)[25, 25, h, h], (int)[0, 0, h / 2 + 4, -h / 2 - 4], T, (double)[90, 90, DEG2RAD(0), DEG2RAD(-60 * dir)], steps); 
+  _execute(A, O, T, phase_diff, steps);
 }
 
 // Otto gait: Flapping
@@ -409,10 +406,11 @@ void Otto9::crusaito(float steps, int T, int h, int dir) {
 //    h: height (Values between 10 - 30)
 //    dir: direction: FOREWARD, BACKWARD
 void Otto9::flapping(float steps, int T, int h, int dir) {
-  //int A[4]= {12, 12, h, h};
-  //int O[4] = {0, 0, h - 10, -h + 10};
-  //double phase_diff[4] = {DEG2RAD(0), DEG2RAD(180), DEG2RAD(-90 * dir), DEG2RAD(90 * dir)};
+  int A[4]= {12, 12, h, h};
+  int O[4] = {0, 0, h - 10, -h + 10};
+  double phase_diff[4] = {DEG2RAD(0), DEG2RAD(180), DEG2RAD(-90 * dir), DEG2RAD(90 * dir)};
   
   // Let's oscillate the servos!
-  _execute((int)[12, 12, h, h], (int)[0, 0, h - 10, -h + 10], T, (double)[DEG2RAD(0), DEG2RAD(180), DEG2RAD(-90 * dir), DEG2RAD(90 * dir)], steps); 
+  _execute(A, O, T, phase_diff, steps);
 }
+
