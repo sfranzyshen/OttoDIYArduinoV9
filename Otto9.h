9b3c27d45815f2e7ddfb3e259cde7633e0a7f177
Scotty Franzyshen <sfranzyshen@gmail.com>
2020-09-25 20:58:26 -0700

    Update Otto9.h
    added humanoid code

--- Otto9.h
+++ Otto9.h
@@ -1,5 +1,5 @@
 // OttoDIY version 0.3.9
-// OttDIY Arduino Project, 2020 | sfranzyshen
+// OttDIY Arduino Project, 2020
 
 #ifndef OTTO9_h
 #define OTTO9_h
@@ -18,16 +18,21 @@
 
 class Otto9 {
   public:
-    void init(int YL, int YR, int RL, int RR, bool load_calibration, 
-		int NoiseSensor, int Buzzer, int USTrigger, int USEcho); // Otto initialization
-    void attachServos(); // Attach & detach functions
+    void init(int YL, int YR, int RL, int RR, bool load_calibration,
+		int NoiseSensor, int Buzzer, int USTrigger, int USEcho); // Otto9 initialization
+    void init(int YL, int YR, int RL, int RR, int LA, int RA, bool load_calibration,
+		int NoiseSensor, int Buzzer, int USTrigger, int USEcho); // Otto9Humanoid initialization
+    void initHUMANOID(int YL, int YR, int RL, int RR,int LA, int RA, bool load_calibration,
+		int NoiseSensor, int Buzzer, int USTrigger, int USEcho); // Otto9Humanoid initialization (depreciated)
+	void attachServos(); // Attach & detach functions
     void detachServos();
-    void setTrims(int YL, int YR, int RL, int RR); // Oscillator Trims
+    void setTrims(int YL, int YR, int RL, int RR); // Oscillator Trims Otto
+	void setTrims(int YL, int YR, int RL, int RR, int LA, int RA); // Oscillator Trims
     void saveTrimsOnEEPROM();
     void moveServos(int time, int servo_target[]); // Predetermined Motion Functions
     void moveSingle(int position,int servo_number);
-    void oscillateServos(int A[4], int O[4], int T, double phase_diff[4], float cycle = 1);
-    void execute(int A[4], int O[4], int T, double phase_diff[4], float steps = 1.0);
+    void oscillateServos(int A[6], int O[6], int T, double phase_diff[6], float cycle = 1);
+    void execute(int A[6], int O[6], int T, double phase_diff[6], float steps = 1.0);
     void home(); // HOME = Otto at rest position
     bool getRestState();
     void setRestState(bool state);
@@ -44,14 +49,18 @@ class Otto9 {
     void moonwalker(float steps = 1, int T = 900, int h = 20, int dir = LEFT);
     void crusaito(float steps = 1, int T = 900, int h = 20, int dir = FORWARD);
     void flapping(float steps = 1, int T = 1000, int h = 20, int dir = FORWARD);
+	void handsup();
+	void handwave(int dir = RIGHT);
+	void loadCalibration();
   private:
-    Oscillator _servo[4];
-    int _servo_pins[4];
-    int _servo_trim[4];
-    int _servo_position[4];
+    Oscillator _servos[6];
+    int _servo_pins[6];
+    int _servo_trims[6];
+    int _servo_positions[6];
+	int _servo_totals;
     unsigned long _final_time;
     unsigned long _partial_time;
-    float _increment[4];
+    float _increment[6];
     bool _isOttoResting;
 };
 
